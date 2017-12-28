896bbbcb058af47bbfa1f114367b175802fb37f9
Thom Morais <thommorais@gmail.com>
2017-12-28 21:34:05 -0200

    update - on mobile when user click on an menu link close menu

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -12,4 +12,9 @@ export default function hamburger() {
     })
 
 
+    menu.addEventListener('click', _ => {
+        menu.classList.toggle('openned')
+        burger.classList.toggle('is-active')
+    })
+
 }
